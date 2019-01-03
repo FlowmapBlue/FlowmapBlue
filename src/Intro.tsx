1771d191f4bd46b7ce389ac84db7099d2712d087
Ilya Boyandin <ilyabo@gmail.com>
2019-01-03 14:08:25 +0100

    Added fork me on GitHub

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -7,23 +7,34 @@ const examples = [
 ]
 
 const Intro = () =>
-  <section className="intro">
-    <h1>Blue Arrow Map - Light</h1>
-    <p>
-      Flow map visualization backed by Google Sheets.
-    </p>
-    <p>
-      Upload your data to Google Sheets to visualize as a flow map.
-    </p>
-    <h2>Examples</h2>
-    <ul>
-      {
-        examples.map(({ key, name }) =>
-          <li key={key}><a href={`/${key}`}>{name}</a></li>
-        )
-      }
-    </ul>
-  </section>
+  <>
+    <section className="intro">
+      <h1>Blue Arrow Map - Light</h1>
+      <p>
+        Flow map visualization backed by Google Sheets.
+      </p>
+      <p>
+        Upload your data to Google Sheets to visualize as a flow map.
+      </p>
+      <h2>Examples</h2>
+      <ul>
+        {
+          examples.map(({ key, name }) =>
+            <li key={key}><a href={`/${key}`}>{name}</a></li>
+          )
+        }
+      </ul>
+    </section>
+
+    <a href="https://github.com/ilyabo/blue-arrow-map-light">
+      <img
+        style={{
+          position: 'absolute', top: 0, right: 0, border: 0,
+        }}
+        src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
+        alt="Fork me on GitHub" />
+    </a>
+  </>
 
 
 export default Intro
