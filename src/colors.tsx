6bac0bcd948fd33f7e002f96e8031d279ee4bd3c
Ilya Boyandin <ilyabo@gmail.com>
2019-09-11 13:18:21 +0200

    Added carto-color colors

--- src/colors.tsx
+++ src/colors.tsx
@@ -46,6 +46,26 @@ const flowColorSchemes: { [key: string]: string[] } = {
   YlGn: asScheme(schemeYlGn),
   YlOrBr: asScheme(schemeYlOrBr),
   YlOrRd: asScheme(schemeYlOrRd),
+  // https://carto.com/carto-colors/
+  Burg: ['#ffc6c4','#f4a3a8','#e38191','#cc607d','#ad466c','#8b3058','#672044'],
+  BurgYl: ['#fbe6c5','#f5ba98','#ee8a82','#dc7176','#c8586c','#9c3f5d','#70284a'],
+  RedOr: ['#f6d2a9','#f5b78e','#f19c7c','#ea8171','#dd686c','#ca5268','#b13f64'],
+  OrYel: ['#ecda9a','#efc47e','#f3ad6a','#f7945d','#f97b57','#f66356','#ee4d5a'],
+  Peach: ['#fde0c5','#facba6','#f8b58b','#f59e72','#f2855d','#ef6a4c','#eb4a40'],
+  PinkYl: ['#fef6b5','#ffdd9a','#ffc285','#ffa679','#fa8a76','#f16d7a','#e15383'],
+  Mint: ['#e4f1e1','#b4d9cc','#89c0b6','#63a6a0','#448c8a','#287274','#0d585f'],
+  BluGrn: ['#c4e6c3','#96d2a4','#6dbc90','#4da284','#36877a','#266b6e','#1d4f60'],
+  DarkMint: ['#d2fbd4','#a5dbc2','#7bbcb0','#559c9e','#3a7c89','#235d72','#123f5a'],
+  Emrld: ['#d3f2a3','#97e196','#6cc08b','#4c9b82','#217a79','#105965','#074050'],
+  BluYl: ['#f7feae','#b7e6a5','#7ccba2','#46aea0','#089099','#00718b','#045275'],
+  Teal: ['#d1eeea','#a8dbd9','#85c4c9','#68abb8','#4f90a6','#3b738f','#2a5674'],
+  TealGrn: ['#b0f2bc','#89e8ac','#67dba5','#4cc8a3','#38b2a3','#2c98a0','#257d98'],
+  Purp: ['#f3e0f7','#e4c7f1','#d1afe8','#b998dd','#9f82ce','#826dba','#63589f'],
+  PurpOr: ['#f9ddda','#f2b9c4','#e597b9','#ce78b3','#ad5fad','#834ba0','#573b88'],
+  Sunset: ['#f3e79b','#fac484','#f8a07e','#eb7f86','#ce6693','#a059a0','#5c53a5'],
+  Magenta: ['#f3cbd3','#eaa9bd','#dd88ac','#ca699d','#b14d8e','#91357d','#6c2167'],
+  SunsetDark: ['#fcde9c','#faa476','#f0746e','#e34f6f','#dc3977','#b9257a','#7c1d6f'],
+  BrwnYl: ['#ede5cf','#e0c2a2','#d39c83','#c1766f','#a65461','#813753','#541f3f'],
 }
 
 export enum ColorScheme {
