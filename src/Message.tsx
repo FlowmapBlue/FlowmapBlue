618642a6026cec207c47bc3a4083f0f5d7c22305
Ilya Boyandin <ilyabo@gmail.com>
2019-10-27 01:13:23 +0200

    Fixed: error fallback text color

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled';
 
-const Message = styled.div`padding: 1em; margin-top: 50px;`
+const Message = styled.div`
+  padding: 1em; 
+  margin-top: 50px;
+  color: #fff;
+`
 
 export default Message
 
