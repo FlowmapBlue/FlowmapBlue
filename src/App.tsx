c26c0d6585f340101574dda976e5b244fc7dac28
ilyabo <ilyabo@gmail.com>
2020-04-06 23:41:39 +0200

    Added FromUrlFlowMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
-import { Router, Route, Switch, RouteComponentProps } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import Home from './Home';
-import * as Sentry from '@sentry/browser';
-import Fallback from './Fallback';
-import { AppToaster } from './AppToaster';
 import { Suspense } from 'react';
+import { Route, RouteComponentProps, Router, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import * as Sentry from '@sentry/browser';
+import Home from './Home';
+import { AppToaster } from './AppToaster';
 import LoadingSpinner from './LoadingSpinner';
 import { SPREADSHEET_KEY_RE } from './constants';
-import { ColorScheme } from './colors';
+import ErrorFallback from './ErrorFallback';
+import ErrorBoundary from './ErrorBoundary';
 
 const GSheetsFlowMap = React.lazy(() => import('./GSheetsFlowMap'));
 const InBrowserFlowMap = React.lazy(() => import('./InBrowserFlowMap'));
+const FromUrlFlowMap = React.lazy(() => import('./FromUrlFlowMap'));
 const ODMatrixConverter = React.lazy(() => import('./ODMatrixConverter'));
 const Geocoding = React.lazy(() => import('./Geocoding'));
 
@@ -29,6 +30,8 @@ const makeGSheetsFlowMap = (embed: boolean) => ({
 }: RouteComponentProps<{ sheetKey: string }>) => (
   <GSheetsFlowMap spreadSheetKey={match.params.sheetKey} embed={embed} />
 );
+
+
 
 export default class App extends React.Component<Props, State> {
   state = {
@@ -52,47 +55,31 @@ export default class App extends React.Component<Props, State> {
       // render fallback UI
       return (
         <Router history={history}>
-          <Fallback>
-            <>
-              Oops… Sorry, but something went wrong.
-              <p>
-                <button
-                  style={{
-                    border: 'none',
-                    color: ColorScheme.primary,
-                    padding: 0,
-                    fontSize: 15,
-                    marginTop: '1em',
-                    textDecoration: 'underline',
-                  }}
-                  onClick={Sentry.showReportDialog}
-                >
-                  Click to report feedback
-                </button>
-              </p>
-            </>
-          </Fallback>
+          <ErrorFallback/>
         </Router>
       );
     } else {
       return (
         <Router history={history}>
-          <Suspense fallback={<LoadingSpinner />}>
-            <Switch>
-              <Route path="/od-matrix-converter" component={ODMatrixConverter} />
-              <Route path="/geocoding" component={Geocoding} />
-              <Route path="/in-browser" component={InBrowserFlowMap} />
-              <Route
-                path={`/:sheetKey(${SPREADSHEET_KEY_RE})/embed`}
-                component={makeGSheetsFlowMap(true)}
-              />
-              <Route
-                path={`/:sheetKey(${SPREADSHEET_KEY_RE})`}
-                component={makeGSheetsFlowMap(false)}
-              />
-              <Route path="/" component={Home} />
-            </Switch>
-          </Suspense>
+          <ErrorBoundary>
+            <Suspense fallback={<LoadingSpinner />}>
+              <Switch>
+                <Route path="/od-matrix-converter" component={ODMatrixConverter} />
+                <Route path="/geocoding" component={Geocoding} />
+                <Route path="/in-browser" component={InBrowserFlowMap} />
+                <Route path="/from-url" component={FromUrlFlowMap} />
+                <Route
+                  path={`/:sheetKey(${SPREADSHEET_KEY_RE})/embed`}
+                  component={makeGSheetsFlowMap(true)}
+                />
+                <Route
+                  path={`/:sheetKey(${SPREADSHEET_KEY_RE})`}
+                  component={makeGSheetsFlowMap(false)}
+                />
+                <Route path="/" component={Home} />
+              </Switch>
+            </Suspense>
+          </ErrorBoundary>
         </Router>
       );
     }
