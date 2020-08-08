591d36b45bd1c19d8bf37625aa31899c85dafabb
Ilya Boyandin <ilyabo@gmail.com>
2020-08-08 21:59:31 +0200

    Adding color scales: magma, inferno, viridis, plasma, cool, warm

--- packages/core/src/colors.ts
+++ packages/core/src/colors.ts
@@ -1,26 +1,32 @@
 import { Colors, DiffColors } from '@flowmap.gl/core';
 import {
-  schemeGnBu,
-  schemePuRd,
-  schemeOrRd,
-  schemePuBu,
-  schemeBuGn,
-  schemePuBuGn,
-  schemeBuPu,
-  schemeRdPu,
-  schemeYlGnBu,
-  schemeYlGn,
-  schemeYlOrBr,
-  schemeYlOrRd,
+  interpolateCool,
+  interpolateInferno,
+  interpolateMagma,
+  interpolatePlasma,
+  interpolateViridis,
+  interpolateWarm,
   schemeBlues,
+  schemeBuGn,
+  schemeBuPu,
+  schemeGnBu,
   schemeGreens,
   schemeGreys,
   schemeOranges,
+  schemeOrRd,
+  schemePuBu,
+  schemePuBuGn,
+  schemePuRd,
   schemePurples,
+  schemeRdPu,
   schemeReds,
+  schemeYlGn,
+  schemeYlGnBu,
+  schemeYlOrBr,
+  schemeYlOrRd,
 } from 'd3-scale-chromatic';
 import { range } from 'd3-array';
-import { scaleSequential, scalePow } from 'd3-scale';
+import { scalePow, scaleSequential } from 'd3-scale';
 import { interpolateRgbBasis } from 'd3-interpolate';
 import { Config } from './types';
 import { hcl } from 'd3-color';
@@ -35,6 +41,11 @@ export enum ColorScheme {
 const FLOW_MIN_COLOR = 'rgba(240,240,240,0.5)';
 export const DEFAULT_COLOR_SCHEME = [FLOW_MIN_COLOR, ColorScheme.primary];
 
+const SCALE_NUM_STEPS = 20;
+const getColorSteps = (interpolate: (x: number) => string) =>
+    range(0,SCALE_NUM_STEPS + 1)
+      .map(i => interpolate(i/SCALE_NUM_STEPS)).reverse();
+
 export const COLOR_SCHEMES: { [key: string]: string[] } = {
   Default: DEFAULT_COLOR_SCHEME,
   // https://carto.com/carto-colors/
@@ -46,17 +57,21 @@ export const COLOR_SCHEMES: { [key: string]: string[] } = {
   BuPu: asScheme(schemeBuPu),
   Burg: ['#ffc6c4', '#f4a3a8', '#e38191', '#cc607d', '#ad466c', '#8b3058', '#672044'],
   BurgYl: ['#fbe6c5', '#f5ba98', '#ee8a82', '#dc7176', '#c8586c', '#9c3f5d', '#70284a'],
+  Cool: getColorSteps(interpolateCool),
   DarkMint: ['#d2fbd4', '#a5dbc2', '#7bbcb0', '#559c9e', '#3a7c89', '#235d72', '#123f5a'],
   Emrld: ['#d3f2a3', '#97e196', '#6cc08b', '#4c9b82', '#217a79', '#105965', '#074050'],
   GnBu: asScheme(schemeGnBu),
   Greens: asScheme(schemeGreens),
   Greys: asScheme(schemeGreys),
+  Inferno: getColorSteps(interpolateInferno),
   Magenta: ['#f3cbd3', '#eaa9bd', '#dd88ac', '#ca699d', '#b14d8e', '#91357d', '#6c2167'],
+  Magma: getColorSteps(interpolateMagma),
   Mint: ['#e4f1e1', '#b4d9cc', '#89c0b6', '#63a6a0', '#448c8a', '#287274', '#0d585f'],
   Oranges: asScheme(schemeOranges),
   OrRd: asScheme(schemeOrRd),
   OrYel: ['#ecda9a', '#efc47e', '#f3ad6a', '#f7945d', '#f97b57', '#f66356', '#ee4d5a'],
   Peach: ['#fde0c5', '#facba6', '#f8b58b', '#f59e72', '#f2855d', '#ef6a4c', '#eb4a40'],
+  Plasma: getColorSteps(interpolatePlasma),
   PinkYl: ['#fef6b5', '#ffdd9a', '#ffc285', '#ffa679', '#fa8a76', '#f16d7a', '#e15383'],
   PuBu: asScheme(schemePuBu),
   PuBuGn: asScheme(schemePuBuGn),
@@ -71,6 +86,8 @@ export const COLOR_SCHEMES: { [key: string]: string[] } = {
   SunsetDark: ['#fcde9c', '#faa476', '#f0746e', '#e34f6f', '#dc3977', '#b9257a', '#7c1d6f'],
   Teal: ['#d1eeea', '#a8dbd9', '#85c4c9', '#68abb8', '#4f90a6', '#3b738f', '#2a5674'],
   TealGrn: ['#b0f2bc', '#89e8ac', '#67dba5', '#4cc8a3', '#38b2a3', '#2c98a0', '#257d98'],
+  Viridis: getColorSteps(interpolateViridis),
+  Warm: getColorSteps(interpolateWarm),
   YlGn: asScheme(schemeYlGn),
   YlGnBu: asScheme(schemeYlGnBu),
   YlOrBr: asScheme(schemeYlOrBr),
